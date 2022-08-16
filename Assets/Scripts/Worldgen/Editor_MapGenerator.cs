325a7673021e6beccb20f15006c5ce8b8dd75ea7
unknown <naani.navis@gmail.com>
2022-08-15 21:01:23 -0400

    started noise map totally not following sebastian lague's tutorial

---
+++ Assets/Scripts/Worldgen/Editor_MapGenerator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+
+public class Editor_MapGenerator : Editor {
+    public override void OnInspectorGUI() {
+        MapGenerator mapGen = (MapGenerator) target;
+
+        if (DrawDefaultInspector()) {
+            if (mapGen.autoUpdate) {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("Generate Map")) {
+            mapGen.GenerateMap();
+        }
+    }
+}
